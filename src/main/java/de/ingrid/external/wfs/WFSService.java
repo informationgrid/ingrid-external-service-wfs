42452dc43af882a56be790d4c66274252431667a
André Wallat <andre.wallat@wemove.com>
2022-01-12 11:34:29 +0100

    Catch NPE when external service failed

--- src/main/java/de/ingrid/external/wfs/WFSService.java
+++ src/main/java/de/ingrid/external/wfs/WFSService.java
@@ -59,7 +59,7 @@ public class WFSService implements GazetteerService {
     public Location getLocation(String locationId, Locale locale) {
         InputStream response = wfsClient.getLocation(locationId, locale);
         Location[] locations = wfsMapper.mapReponseToLocations( response );
-        return locations.length > 0 ? locations[0] : null;
+        return (locations != null && locations.length > 0) ? locations[0] : null;
     }
 
     @Override
